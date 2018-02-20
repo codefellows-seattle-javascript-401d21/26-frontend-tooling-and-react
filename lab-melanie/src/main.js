bc3e84a13eb2712576d96d17b585046a0f30282c
Melanie Downing <sayanything830@gmail.com>
2018-02-19 18:58:56 -0800

    stretch goal done

--- lab-melanie/src/main.js
+++ lab-melanie/src/main.js
@@ -3,7 +3,7 @@
 import './styles/main.scss';
 
 // const React = require('react')
-import * as cowsay from 'cowsay';
+import {say, KITTY, ELEPHANT, DRAGON, SHEEP, TURTLE, SQUIRREL} from 'cowsay';
 import faker from 'faker';
 import React from 'react';
 import ReactDom from 'react-dom';
@@ -25,19 +25,43 @@ class Header extends React.Component {
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = cowsay.say({
-      text: `${faker.hacker.phrase()}`,
-    });
+    this.state = {
+      content: faker.hacker.phrase(),
+      cow: '',
+    };
 
     this.handleClick = this.handleClick.bind(this);
+    this.selectClick = this.selectClick.bind(this);
   }
 
   handleClick() {
     this.setState({
-      content: cowsay.say({
-        text: `${faker.hacker.phrase()}`,
-      }),
+      content: faker.hacker.phrase(),
     });
+  }
+
+  selectClick(event) {
+    if(event.target.value === 'COW') {
+      this.setState({cow: ''});
+    }
+    if(event.target.value === 'KITTY') {
+      this.setState({cow: KITTY});
+    }
+    if(event.target.value === 'ELEPHANT') {
+      this.setState({cow: ELEPHANT});
+    }
+    if(event.target.value === 'DRAGON') {
+      this.setState({cow: DRAGON});
+    }
+    if(event.target.value === 'SHEEP') {
+      this.setState({cow: SHEEP});
+    }
+    if(event.target.value === 'TURTLE') {
+      this.setState({cow: TURTLE});
+    }
+    if(event.target.value === 'SQUIRREL') {
+      this.setState({cow: SQUIRREL});
+    }
   }
 
   render() {
@@ -45,8 +69,20 @@ class App extends React.Component {
       <div className="app">
         <Header />
         <h2>Generate Cowsay Lorem</h2>
+        <select onChange={this.selectClick}>
+          <option value='COW'>Cow</option>
+          <option value='KITTY'>Kitty</option>
+          <option value='ELEPHANT'>Elephant</option>
+          <option value='DRAGON'>Dragon</option>
+          <option value='SHEEP'>Sheep</option>
+          <option value='TURTLE'>Turtle</option>
+          <option value='SQUIRREL'>Squirrel</option>
+        </select>
         <button onClick={this.handleClick}>Click Me</button>
-        <pre>{this.state.content}</pre>
+        <pre>{say({
+          text: this.state.content,
+          cow: this.state.cow,
+        })}</pre>
       </div>
     );
   }
